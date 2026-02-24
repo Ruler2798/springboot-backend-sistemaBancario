b7c931f517bc6bc6b461c98d6f9e6996a45232ac
Luis <lphg3940@gmail.com>
2026-02-24 11:43:51 -0600

    fix:arreglo de pom

--- WebApp/src/main/java/org/luishernandez/WebApp/WebAppApplication.java
+++ WebApp/src/main/java/org/luishernandez/WebApp/WebAppApplication.java
@@ -1,13 +1,9 @@
-package org.luishernandez.WebApp;
+package org.luishernandez.webApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = "org.luishernandez.WebApp.model")
-@EnableJpaRepositories(basePackages = "org.luishernandez.WebApp.repository")
 public class WebAppApplication {
 
     public static void main(String[] args) {
