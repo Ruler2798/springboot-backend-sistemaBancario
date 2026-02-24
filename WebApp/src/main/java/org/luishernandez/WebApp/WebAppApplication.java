8e50f683e23467ac5924057f68adb860547bd533
Luis <lphg3940@gmail.com>
2026-02-24 10:26:51 -0600

    fix:Se areglo el model por problemas de compatibilidad

--- WebApp/src/main/java/org/luishernandez/WebApp/WebAppApplication.java
+++ WebApp/src/main/java/org/luishernandez/WebApp/WebAppApplication.java
@@ -2,12 +2,15 @@ package org.luishernandez.WebApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "org.luishernandez.WebApp.model")
+@EnableJpaRepositories(basePackages = "org.luishernandez.WebApp.repository")
 public class WebAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebAppApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppApplication.class, args);
+    }
 }
